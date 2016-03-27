7699a66c46e226dc523d1553dc357ee55b88eb45
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-27 16:42:49 -0700

    fix typo in path

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path')
 
 module.exports = cdm
 
-function cdm(path, cb) {
-  fs.mkdir(path, function(error) {
-    if (error) {
-      fs.stat(p, function(error, stats) {
-        process.chdir(p)
+const cdm = (path, cb) => {
+  fs.mkdir(path, (err) => {
+    if (err) {
+      fs.stat(path, (err, stats) => {
+        process.chdir(path)
       })
     }  
   }) 
